9125a2f26637463a43c522d4a96c0876f1acbce5
Truong Huu Thanh <truonghuuthanh95@gmail.com>
2021-09-14 22:49:43 +0700

    commit

--- back-end/Models/DAOs/DynamicSurveyDBContext.cs
+++ back-end/Models/DAOs/DynamicSurveyDBContext.cs
@@ -29,11 +29,7 @@ namespace back_end.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=10.200.6.172;Database=DynamicSurveyDB;User Id=sa;Password=Meocon0710@;");
-            }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
